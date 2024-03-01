491be839f5d6a74611c91eed79f9bf99fbc47409
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-02-29 16:15:02 -0800

    removed original vite code and edited mainjsx to handle proper pages

--- alex-sierra-react-portfolio/src/main.jsx
+++ alex-sierra-react-portfolio/src/main.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx';
+import ErrorPage from './pages/ErrorPage';
+import ContactPage from './pages/ContactPage';
+import Portfolio from './pages/Portfolio';
+import AboutPage from './pages/AboutPage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
